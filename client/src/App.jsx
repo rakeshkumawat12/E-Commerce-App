1510e0cbd006b6558e5e514b7855ed91483c2da1
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-08-01 18:01:03 +0530

    Added Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
 import './App.css';
-// import Home from './pages/home/Home';
-// import Cart from './pages/cart/Cart';
-// import Login from './pages/login/Login';
-// import Register from './pages/register/Register';
-// import Product from './pages/product/Product';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Cart from './pages/cart/Cart';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Product from './pages/product/Product';
 import ProductList from './pages/productlist/ProductList';
+
 
 function App() {
   return (
-    <div className="App">
-      {/* <Home/> */}
-      {/* <Cart/> */}
-      {/* <Login/> */}
-      {/* <Register/> */}
-      {/* <Product/> */}
-      <ProductList/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/cart' element={<Cart />} exact />
+          <Route path='/login' element={<Login />} exact />
+          <Route path='register' element={<Register />} exact />
+          <Route path='/product' element={<Product />} exact />
+          <Route path='/productlist' element={<ProductList />} exact />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
